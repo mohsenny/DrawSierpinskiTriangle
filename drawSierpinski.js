17ed19cbc4819f722896f9ec8087a93c27b23c14
Mohsen <mohsen.n89@gmail.com>
2022-06-24 20:07:33 +0200

    Mino fixes

--- drawSierpinski.js
+++ drawSierpinski.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var drawing = require('pngjs-draw');
 var png = drawing(require('pngjs').PNG);
  
-fs.createReadStream("blue.png")
+fs.createReadStream('sierpinski.png')
   .pipe(new png({ filterType: 4 }))
   .on('parsed', function() {
     const offset = 0
@@ -11,7 +11,8 @@ fs.createReadStream("blue.png")
     	[offset, 200],
     	[250, 200]
     ]
-    // draw the traingle
+    
+    // draw the traingle edges
     this.drawPixel(trianglePoints[0][0], trianglePoints[0][1], this.colors.black())
     this.drawPixel(trianglePoints[1][0], trianglePoints[1][1],  this.colors.black())
     this.drawPixel(trianglePoints[2][0], trianglePoints[2][1],  this.colors.black())
@@ -22,23 +23,21 @@ fs.createReadStream("blue.png")
 
     let randomEdge = ''
     let lastDot = firstDot
- 	for (let i = 0; i < 20000; i++) {
- 		randomEdge = chooseRandomEdge();
- 		console.log(randomEdge)
- 		newDot = calculateNewDot(lastDot, randomEdge, trianglePoints);
-  		console.log("-----")
-    	this.drawPixel(newDot[0], newDot[1], this.colors.red());
-    	lastDot = newDot;
- 	}
+    
+ 	  for (let i = 0; i < 20000; i++) {
+ 		   randomEdge = chooseRandomEdge();
+ 		   newDot = calculateNewDot(lastDot, randomEdge, trianglePoints);
+    	 this.drawPixel(newDot[0], newDot[1], this.colors.red());
+     	lastDot = newDot;
+ 	  }
 
     // Writes file
-    this.pack().pipe(fs.createWriteStream('blue.out.png'));
-
+    this.pack().pipe(fs.createWriteStream('sierpinski.out.png'));
   });
 
   function chooseRandomEdge() {
   	items = [0, 1, 2]
- 	return items[Math.floor(Math.random()*items.length)]
+ 	 return items[Math.floor(Math.random()*items.length)]
   }
 
   function getFirstDotCoordinate() {
@@ -50,6 +49,5 @@ fs.createReadStream("blue.png")
   	console.log(trianglePoints[edge])
   	const x = (lastDot[0] + trianglePoints[edge][0]) / 2;
   	const y = (lastDot[1] + trianglePoints[edge][1]) / 2;
-
-  	console.log([Math.trunc(x), Math.trunc(y)])
+  	return [Math.trunc(x), Math.trunc(y)];
   }
