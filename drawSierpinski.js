3f4300c7ca38f342251b1f82fe1d8ec997c66fc0
Mo Nasiri <mohsen.n89@gmail.com>
2022-08-19 13:46:45 +0200

    Indentions fixed

--- drawSierpinski.js
+++ drawSierpinski.js
@@ -5,25 +5,25 @@ var png = drawing(require('pngjs').PNG);
 fs.createReadStream('sierpinski.png')
   .pipe(new png({ filterType: 4 }))
   .on('parsed', function() {
-    const offset = 0
+    const offset = 0;
     const trianglePoints = [
     	[125, offset],
     	[offset, 200],
     	[250, 200]
-    ]
+    ];
     
     // draw the traingle edges
-    this.drawPixel(trianglePoints[0][0], trianglePoints[0][1], this.colors.black())
-    this.drawPixel(trianglePoints[1][0], trianglePoints[1][1],  this.colors.black())
-    this.drawPixel(trianglePoints[2][0], trianglePoints[2][1],  this.colors.black())
+    this.drawPixel(trianglePoints[0][0], trianglePoints[0][1], this.colors.black());
+    this.drawPixel(trianglePoints[1][0], trianglePoints[1][1],  this.colors.black());
+    this.drawPixel(trianglePoints[2][0], trianglePoints[2][1],  this.colors.black());
  
     // draw the first dot
-    firstDot = getFirstDotCoordinate()
-    this.drawPixel(firstDot[0], firstDot[1], this.colors.green())
+    firstDot = getFirstDotCoordinate();
+    this.drawPixel(firstDot[0], firstDot[1], this.colors.green());
 
     // draw the rest in a loop
-    let randomEdge = ''
-    let lastDot = firstDot
+    let randomEdge = '';
+    let lastDot = firstDot;
     
     for (let i = 0; i < 25000; i++) {
       randomEdge = chooseRandomEdge();
@@ -34,19 +34,20 @@ fs.createReadStream('sierpinski.png')
 
     // Writes file
     this.pack().pipe(fs.createWriteStream('sierpinski.out.png'));
-  });
-
-  function chooseRandomEdge() {
-    items = [0, 1, 2]
-    return items[Math.floor(Math.random()*items.length)]
   }
+);
 
-  function getFirstDotCoordinate() {
-    return [180, 150]
-  }
+function chooseRandomEdge() {
+  items = [0, 1, 2];
+  return items[Math.floor(Math.random()*items.length)];
+}
 
-  function calculateNewDot(lastDot, edge, trianglePoints) {
-    const x = (lastDot[0] + trianglePoints[edge][0]) / 2;
-    const y = (lastDot[1] + trianglePoints[edge][1]) / 2;
-    return [Math.trunc(x), Math.trunc(y)];
-  }
+function getFirstDotCoordinate() {
+  return [180, 150];
+}
+
+function calculateNewDot(lastDot, edge, trianglePoints) {
+  const x = (lastDot[0] + trianglePoints[edge][0]) / 2;
+  const y = (lastDot[1] + trianglePoints[edge][1]) / 2;
+  return [Math.trunc(x), Math.trunc(y)];
+}
